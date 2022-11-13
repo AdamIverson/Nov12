9a4e81d387a363a306d547c8901063022b0fbf2e
Adam Iverson <aciverso@gmail.com>
2022-11-13 08:40:01 -0600

    about page name

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './components/HomeScreen';
-import About from './components/Profile';
+import About from './components/About';
 
 const Stack = createNativeStackNavigator();
 

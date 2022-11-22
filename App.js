51be893bbdf18c425dd3c7ad1a80771fec7160b4
Adam Iverson <aciverso@gmail.com>
2022-11-22 11:14:18 -0600

    paper/vector

--- App.js
+++ App.js
@@ -1,25 +1,32 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './components/HomeScreen';
 import About from './components/About';
 import Contact from './components/ContactScreen';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const App = () => {
 
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-      <Stack.Screen
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="About" component={About} />
+        <Tab.Screen name="Contact" component={Contact} />
+      </Tab.Navigator>
+      {/* <Stack.Navigator>
+        <Stack.Screen
           name="Home"
           component={HomeScreen}
           options={{ title: 'Welcome' }}
         />
-        <Stack.Screen name="About" component={About} options={{ title: 'aboot'}}/>
-        <Stack.Screen name="Contact" component={Contact} options={{ title: 'contact us please contact us' }}/>
-      </Stack.Navigator>
+        <Stack.Screen name="About" component={About} options={{ title: 'aboot' }} />
+        <Stack.Screen name="Contact" component={Contact} options={{ title: 'contact us please contact us' }} />
+      </Stack.Navigator> */}
     </NavigationContainer>
   );
 };
